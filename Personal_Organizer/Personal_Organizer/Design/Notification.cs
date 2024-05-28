c509ee227a2278ffded45b2df2e30a66c7621a53
CerenAdiyaman <cerenadiyamn@gmail.com>
2024-05-28 23:30:01 +0300

    reminder improved

---
+++ Personal_Organizer/Personal_Organizer/Design/Notification.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_Organizer.Design
+{
+    public partial class Notification : Form
+    {
+        int interval = 0;
+        public Notification()
+        {
+            InitializeComponent();
+        }
+
+        private void PositionNotBox()
+        {
+            int xPos = 0; int yPos = 0;
+            xPos = Screen.GetWorkingArea(this).Width;
+            yPos = Screen.GetWorkingArea(this).Height;
+            this.Location = new Point(xPos-this.Width, yPos-this.Height);
+
+        }
+
+        private void Notification_Load(object sender, EventArgs e)
+        {
+            PositionNotBox();
+            for (int i = 0; i<673; i++)
+            {
+                timer1.Start();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            linPanel.Width = linPanel.Width +2;
+            if(linPanel.Width == 673)
+            {
+               // this.Close();
+            }
+        }
+
+    }
+}
